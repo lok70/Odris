cc6a01152122243f3198d9cc2870dfbdc86453b5
bekoe1 <zabulimya@gmail.com>
2024-05-19 18:01:11 +0300

    fixed some errors
    lshift fix + new mini boss

--- Assets/Scripts/PlayerLogic/DashPlayerScript.cs
+++ Assets/Scripts/PlayerLogic/DashPlayerScript.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class DashPlayerScript : BasePlayerController
 {
-
     private Vector2 dashDirection;
     private static bool dashFlag;
 
-
     [SerializeField] private float dashDistance = 50f;
+    [SerializeField] private float bounceDistance = 20f; // Новая переменная для расстояния отскока
 
+    private void Start()
+    {
+        canDash = true;
+    }
 
     private void Update()
     {
@@ -18,7 +21,7 @@ public class DashPlayerScript : BasePlayerController
         dashDirection = (mousePos - rb.position).normalized;
         if (Input.GetKey(KeyCode.LeftShift) && canDash)
         {
-
+            Debug.Log("Shift pressed");
             canDash = false;
             dashFlag = true;
         }
@@ -26,19 +29,28 @@ public class DashPlayerScript : BasePlayerController
 
     private void FixedUpdate()
     {
-        if (dashFlag)
+        if (dashFlag && !isDashing)
         {
+            Debug.Log("Dashing done");
             isDashing = true;
             rb.velocity = Vector2.zero;
-            rb.AddForce(-dashDirection * dashDistance, ForceMode2D.Impulse);
+            //rb.AddForce(-dashDirection * dashDistance, ForceMode2D.Impulse);
+            rb.MovePosition(Vector3.MoveTowards(transform.position, -dashDirection*dashDistance, bounceDistance* Time.deltaTime));
+
             StartCoroutine(dashTimer());
-            dashFlag = false;
         }
     }
+
     private IEnumerator dashTimer()
     {
         yield return new WaitForSeconds(1.5f);
         canDash = true;
         isDashing = false;
+        dashFlag = false;
+
+        // Добавляем отскок
+        Vector2 bounceDirection = -dashDirection; // Направление отскока - в противоположную сторону
+        rb.velocity = Vector2.zero;
+        rb.AddForce(bounceDirection * bounceDistance, ForceMode2D.Impulse);
     }
 }
