2cc29aba90dcf8227561f98fe6046c08b0894866
lok70 <lokwer321@gmail.com>
2024-05-19 21:52:14 +0300

    trying tot merge

---
+++ Assets/Scripts/Inventory/ArmorScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmorScript : MonoBehaviour
+{
+    Item item;
+    InventorySlot currslot;
+
+    [SerializeField] public float DamageReduceMultiplier = 1;
+
+    void Start()
+    {
+        item = null;
+        currslot = transform.GetChild(0).GetComponent<InventorySlot>();
+    }
+
+    void Update()
+    {
+        if (transform.childCount != 0)
+        {
+            currslot = transform.GetChild(0).GetComponent<InventorySlot>();
+            if (currslot.slotItem != item)
+            {
+                item = currslot.slotItem;
+                if (item != null)
+                {
+                    UpdateMultiplier();
+                }
+                else DamageReduceMultiplier = 1;
+            }
+        }
+    }
+
+    void UpdateMultiplier()
+    {
+        if (item.GetType() == typeof(Armor))
+        {
+            DamageReduceMultiplier = (item as Armor).EarnedDamageReduceMultiplier;
+        }
+        else DamageReduceMultiplier = 1;
+    }
+}
---
+++ Assets/Scripts/Inventory/Sword.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "Sword", menuName = "Inventory/Sword", order = 2)]
+
+public class Sword : Item
+{
+    public float DamageMultiplier;
+
+}
+
